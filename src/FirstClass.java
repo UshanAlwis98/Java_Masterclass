6650054cf0bff86743f58241a50871fd50707fda
Ushan Alwis Github <104507543+UshanAlwis98@users.noreply.github.com>
2025-04-01 00:14:31 +0530

    Data type Learned.

--- src/FirstClass.java
+++ src/FirstClass.java
@@ -1,6 +1,10 @@
 public class FirstClass {
     public static void main(String[] args) {
-        System.out.print("Hola");
+       long myLongValue = 2147483648;  // Integer Maximum value is 214_748_3647. Java is taking this number as integer by default.
+       System.out.print(myLongValue);   // Putting Simple l or Capital L is the Solution for this.
+
+       short myShortValue = 32768;   // Maximum short number is 32767
+
     }
 
 }
