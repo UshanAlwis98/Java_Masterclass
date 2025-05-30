b007db88c88bb7551db4c4160892013e446970b8
Ushan Alwis Github <104507543+UshanAlwis98@users.noreply.github.com>
2025-05-31 00:29:48 +0530

    Default values for fields on Car.java class applied.

--- src/Car.java
+++ src/Car.java
@@ -1,13 +1,13 @@
 public class Car {
 
-    private String make;
-    private  String model;
-    private String color;
+    private String make = "Benz";
+    private  String model = "C class";
+    private String color = "Black";
     private int doors;
     private  boolean convirtable;
 
     public void describeCar() {
-        System.out.println(doors + "-Door" +
+        System.out.println(doors + "-Door " +
                             color + " " +
                 make + " " +
                 model + " " +
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World! Ushan here");
+       Car car = new  Car();
+       car.describeCar();
     }
 }
