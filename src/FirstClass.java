9e08909c25b5029142c1ed92a71955f8207c3a4f
Ushan Alwis Github <104507543+UshanAlwis98@users.noreply.github.com>
2025-05-30 21:46:08 +0530

    First Class java problem solved.

--- src/FirstClass.java
+++ src/FirstClass.java
@@ -1,18 +1,6 @@
 public class FirstClass {
     public static void main(String[] args) {
-        // Data Type Understanding
-
-       long myLongValue = 2147483648;  // Integer Maximum value is 214_748_3647. Java is taking this number as integer by default.
-       System.out.print(myLongValue);   // Putting Simple l or Capital L is the Solution for this.
-
-       short myShortValue = 32768;   // Maximum short number is 32767
-
-
-        // Java Casting
-        short ourShortValue =  Short.MIN_VALUE; int ourIntValue = Integer.MAX_VALUE; // EKAMA STATMENT eke datatypes 2 k thiyyedi mehema danna
-
-
-
+      System.out.print("Hello World");
     }
 
 }
