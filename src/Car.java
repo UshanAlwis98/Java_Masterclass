b73cd3b0daf34a4bc792b1c76d647b7b5be6cf1d
Ushan Alwis Github <104507543+UshanAlwis98@users.noreply.github.com>
2025-06-05 00:06:22 +0530

    My example demonstrated.

--- src/Car.java
+++ src/Car.java
@@ -1,58 +1,23 @@
 public class Car {
 
-    private String make = "Tesla";
-    private  String model = "Model X";
-    private String color = "Red";
-    private int doors = 4;
-    private  boolean convertible = false;
+        private String make = "Toyota";
+        private String model;
 
-    public String getMake() {
-        return make;
-    }
+        private int doors;
+        private int passengers;
 
-    public void setMake(String make) {
-        this.make = make;
-    }
+        private boolean convertible;
+        private  double engineCapacity;
 
-    public String getModel() {
-        return model;
-    }
 
-    public void setModel(String model) {
-        this.model = model;
-    }
+        public String getMake(){
+            return make;
+        }
 
-    public String getColor() {
-        return color;
-    }
+        public  void setMake(String mid){
+            this.make=mid;
+        }
 
-    public void setColor(String color) {
-        this.color = color;
-    }
-
-    public int getDoors() {
-        return doors;
-    }
-
-    public void setDoors(int doors) {
-        this.doors = doors;
-    }
-
-    public boolean isConvertible() {
-        return convertible;
-    }
-
-    public void setConvertible(boolean convertible) {
-        this.convertible = convertible;
-    }
-
-    public void describeCar() {
-        System.out.println( doors + "-Door " +
-                            color + " " +
-                            make + " " +
-                            model + " " +
-                            (convertible ? "convertible" : ""));
-    }
 
 
 }
--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-       Car car = new  Car();
-
-        car.setMake("Ferrari");
-        System.out.println("Make is = " + car.getMake());
-
-        car.setModel("Monza Sprider");
-        System.out.println("Model is = " + car.getModel());
-
-        car.setColor("Light grey");
-        System.out.println("Color is = " + car.getColor());
-
-        car.setDoors(7);
-        System.out.println(car.getDoors() + " -doors");
-
-        car.setConvertible(true);
-        System.out.println("This car is " + car.isConvertible());
-
-//        car.describeCar();
+        Car myFirstCarObject = new Car();
+        myFirstCarObject.setMake("Benz");
+        System.out.println(myFirstCarObject.getMake());
     }
 }
