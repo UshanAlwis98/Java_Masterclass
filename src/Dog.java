b8b1ca4b4cf4663046f31a274979356fbf9fffa7
Ushan Alwis Github <104507543+UshanAlwis98@users.noreply.github.com>
2025-06-12 00:16:57 +0530

    Static variable and instance variable are recognized.

---
+++ src/Dog.java
@@ -0,0 +1,10 @@
+public class Dog {
+    private static String name;
+     public Dog(String name){
+         Dog.name = name;
+     }
+
+     public void printName(){
+         System.out.println("Name= " + name);
+     }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,10 @@
 public class Main {
     public static void main(String[] args) {
 
-        CoffeeCup cupA = new CoffeeCup("Medium", "Vanilla");
-        CoffeeCup cupB = cupA;
-
-        cupB.setFlavor("Hazelnut");
-
-        CoffeeCup cupC = new CoffeeCup("Large", "Mocha");
-        cupB = cupC;
-
-        cupC.setSize("Small");
-
-        cupA.printDetails("Cup A");
-        cupB.printDetails("Cup B");
-        cupC.printDetails("Cup C");
-
-        // Your Challenge:
-        // 1. Predict the output.
-        // 2. Identify:
-        //    - How many objects are created?
-        //    - How many references?
-        //    - Which variables refer to the same object?
+        Dog rex = new Dog("rex");
+        Dog fluffy = new Dog ("fluffy");
+        rex.printName();
+        fluffy.printName();
     }
 }
 
