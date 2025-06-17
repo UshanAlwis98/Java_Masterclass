a8bda1e6a4e162ace304f80d07c303fea638ea5a
Ushan Alwis Github <104507543+UshanAlwis98@users.noreply.github.com>
2025-06-17 19:59:39 +0530

    Complex Number challenge accepted.

---
+++ src/ComplexNumber.java
@@ -0,0 +1,9 @@
+public class ComplexNumber {
+    private double real;
+    private double imaginary;
+
+    public ComplexNumber(double real, double imaginary) {
+        this.real = real;
+        this.imaginary = imaginary;
+    }
+}
