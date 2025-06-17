a24a3ef21564624f199cb046b42c2dc15c9f1b5d
Ushan Alwis Github <104507543+UshanAlwis98@users.noreply.github.com>
2025-06-17 20:43:05 +0530

    Complex Number challenge solved.

--- src/ComplexNumber.java
+++ src/ComplexNumber.java
@@ -2,8 +2,43 @@ public class ComplexNumber {
     private double real;
     private double imaginary;
 
+    // Constructor
     public ComplexNumber(double real, double imaginary) {
         this.real = real;
         this.imaginary = imaginary;
+    }
+
+    // Getter for real
+    public double getReal() {
+        return this.real;
+    }
+
+    // Getter for imaginary
+    public double getImaginary() {
+        return this.imaginary;
+    }
+
+    // Add using real and imaginary values
+    public void add(double real, double imaginary) {
+        this.real += real;
+        this.imaginary += imaginary;
+    }
+
+    // Add using another ComplexNumber object
+    public void add(ComplexNumber complexNumber) {
+        this.real += complexNumber.getReal();
+        this.imaginary += complexNumber.getImaginary();
+    }
+
+    // Subtract using real and imaginary values
+    public void subtract(double real, double imaginary) {
+        this.real -= real;
+        this.imaginary -= imaginary;
+    }
+
+    // Subtract using another ComplexNumber object
+    public void subtract(ComplexNumber complexNumber) {
+        this.real -= complexNumber.getReal();
+        this.imaginary -= complexNumber.getImaginary();
     }
 }
