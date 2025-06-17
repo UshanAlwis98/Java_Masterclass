d8922d92448db450960263f97fcdcf03127507e6
Ushan Alwis Github <104507543+UshanAlwis98@users.noreply.github.com>
2025-06-17 19:27:10 +0530

    getArea method done.

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,8 @@
 public class Main {
     public static void main(String[] args) {
 
-        Point first = new Point(6, 5);
-        Point second = new Point(3, 1);
-        System.out.println("distance(0,0)= " + first.distance());
-        System.out.println("distance(second)= " + first.distance(second));
-        System.out.println("distance(2,2)= " + first.distance(2, 2));
-        Point point = new Point();
-        System.out.println("distance()= " + point.distance());
+       Floor floor = new Floor(8.1,6.5);
+       System.out.println("Area is " + floor.getArea());
 
 
 
