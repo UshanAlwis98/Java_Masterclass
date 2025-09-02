da286275e5784869dc9803a51ab636bab8742236
Ushan Alwis Github <104507543+UshanAlwis98@users.noreply.github.com>
2025-09-02 20:41:56 +0530

    Challenge done

--- src/ChallangeInheritance/SalariedEmployee.java
+++ src/ChallangeInheritance/SalariedEmployee.java
@@ -5,6 +5,7 @@ public class SalariedEmployee extends Employee {
     private double annualSalary;
     private boolean isRetired;
 
+    //Constructor of SalariedEmployee
     public SalariedEmployee(String name, String birthDate, String hireDate, double annualSalary) {
         super(name, birthDate, hireDate);
         this.annualSalary = annualSalary;
