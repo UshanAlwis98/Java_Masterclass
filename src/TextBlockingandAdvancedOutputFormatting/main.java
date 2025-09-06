d4ae84652fe3f6a49325166add5a283993f6c195
Ushan Alwis Github <104507543+UshanAlwis98@users.noreply.github.com>
2025-09-06 08:35:11 +0530

    Advanced output Formatting.

---
+++ src/TextBlockingandAdvancedOutputFormatting/main.java
@@ -0,0 +1,13 @@
+package TextBlockingandAdvancedOutputFormatting;
+
+public class main {
+
+    public static void main(String [] args){
+
+        String bulletIt = "Print a Bulleted list \n" +
+                "\t\t\t\t\t\u2022 First Bullet \n"+
+                "\t\t\t\t\t\u2022 Second Bullet";
+
+        System.out.print(bulletIt);
+    }
+}
