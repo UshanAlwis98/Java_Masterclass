bedf21d309cbf90de081cb065bb67a4cbba962cc
Ushan Alwis Github <104507543+UshanAlwis98@users.noreply.github.com>
2025-09-14 17:30:45 +0530

    Composition Applied.

--- src/BedroomChallangeComposition/Main.java
+++ src/BedroomChallangeComposition/Main.java
@@ -5,6 +5,7 @@ public class Main {
     public static void main(String [] args){
 
         Wall wall1 = new Wall("West");
+
         Wall wall2 = new Wall("East");
         Wall wall3 = new Wall("South");
         Wall wall4 = new Wall("North");
